338ed09770b6a549d8e3f22f9ce2981df1405beb
Shahab <hajinasiri@gmail.com>
2018-06-24 08:33:47 -0700

    setting the template

---
+++ includes/first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <link rel="stylesheet" href="styles/styles.css" type="text/css">
+  <?php include 'includes/_header.php'?>
+</header>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
