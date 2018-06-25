120bfe73f80ae64538efe21cb48499d1bb49fa18
Shahab <hajinasiri@gmail.com>
2018-06-24 21:19:30 -0700

    Putting jquery in a separate file

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./includes/create.js"></script>
 </head>
 <body>
 
@@ -16,7 +17,7 @@
     <div class="form-group">
 
          <form name="add_name" id="add_name">
-          <div><input type="text" name="email" placeholder="Enter your email" class="email" /></div>
+          <div><input id="emailForm" type="email" name="email" placeholder="Enter your email" class="email" /></div>
           <div><input type="text" name="event" placeholder="Enter the event's name" class="event" /></div>
 
             <div class="table-responsive">
@@ -33,13 +34,14 @@
 </div>
 
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
     $(document).ready(function(){
       var postURL = "/addmore.php";
       var i=1;
 
       $('#add').click(function(){
         let user_input = $(this).parent().siblings().children('#input').val();
+        user_input = String(user_input);
         if (user_input !== ''){
           i++;
           $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added"><td><input type="text" value='+user_input+' name="name[]" class="form-control name_list" required /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
@@ -48,7 +50,6 @@
 
       });
 
-
       $(document).on('click', '.btn_remove', function(){
            var button_id = $(this).attr("id");
            $('#row'+button_id+'').remove();
@@ -56,6 +57,8 @@
 
 
       $('#submit').click(function(){
+
+        console.log($('#add_name').serialize())
            $.ajax({
                 url:postURL,
                 method:"POST",
@@ -74,5 +77,5 @@
 
     });
 </script>
-</body>
+ --></body>
 </html>
