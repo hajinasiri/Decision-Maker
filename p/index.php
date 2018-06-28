eb67b8227d7498903adb7d244837d8c032dfd173
Shahab <hajinasiri@gmail.com>
2018-06-28 15:38:16 -0700

    all the views are separated and put in view folder

--- p/index.php
+++ p/index.php
@@ -1,5 +1,7 @@
 <?php
-include '../includes/Tables.php';
+  session_start();
+  include '../includes/Tables.php';
+  include '../includes/ChromePhp.php';
 
   $user = 'root';
   $pass = '';
@@ -18,26 +20,11 @@ include '../includes/Tables.php';
   $choices = $choices_data[0];
   $ranks = serialize($choices_data[1]);
   $choice_ids = serialize($choices_data[2]);
-
+  //setting the session and redirecting to the view page
+  $_SESSION['choices'] = $choices ;
+  $_SESSION['question'] = $question ;
+  $_SESSION['result_link'] = $result_link;
+  header("Location: http://localhost:8080/dmaker/views/vote.php");
+  exit;
   $db->close();
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-  <div>Vote for each choice with a number between 1 and <?php echo (sizeof($choices)) ?></div>
-  <form name="votes" method="post" action="../thanks.php">
-    <input type="hidden" name="result_link" value="<?php echo $result_link?>" />
-    <input type="hidden" name="choice_ids" value="<?php echo htmlentities($choice_ids) ?>" />
-    <input type="hidden" name="ranks" value="<?php echo htmlentities($ranks) ?>" />
-    <?php
-    for($i=0; $i<sizeof($choices); $i++){
-      echo "<div>
-          <span>".$choices[$i]."</span>
-          <span><input type='text' name='vote[]'' placeholder='Enter an option' class='vote'/>
-          </div>";
-    }
-    ?>
-    <input type="submit" name="submit" id="submit" class="submit" value="Submit" />
-  </form>
-  </div>
-<body>
