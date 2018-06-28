b07e8dae7f673d00e298493eb5d8e9f5f552b3d2
Shahab <hajinasiri@gmail.com>
2018-06-27 17:07:00 -0700

    vote and result links are shown on the page

--- completed.php
+++ completed.php
@@ -2,13 +2,7 @@
   include "./includes/ChromePhp.php";
   include "./includes/Tables.php";
 
-
-
-  $str = file_get_contents("settings.json");
-  $json = json_decode($str, true);
-  echo ($json['password']);
-
-   function create_url() {
+  function create_url() {
     $characters = 'ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuv1234567890';
     $url = '';
     for ($x = 0; $x < 6; $x++) {
@@ -17,8 +11,6 @@
     }
     return $url;
   }
-
-
   //Setting parameters for connecting to the database
   $user = 'root';
   $pass = '';
@@ -31,8 +23,6 @@
   Tables::create_users($db);
   Tables::create_poles($db);
   Tables::create_choices($db);
-
-
 
   $email = $_POST['email'];
   $user_id = Tables::insert_user($db,$email);
@@ -47,11 +37,19 @@
     Tables::insert_choices($db,$pole_id,$choice);
   }
 
-
-
-
-
-
-
   $db->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<h2> Your pole is created</h2>
+<div>Here is the link which you can send the participant to vot</div>
+<a href= <?php
+        echo "localhost:8080/p/".$vote_link;
+        ?>
+> Pole link</a>
+<div>and here is the link where you can see the result</div>
+<a href= <?php
+        echo "localhost:8080/p/".$result_link;
+        ?>
+> Result link</a>
